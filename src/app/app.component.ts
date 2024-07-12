4a998bbbfe1d8f11d48113e123929f13e32c2d72
gpdoud <gpdoud@gmail.com>
2024-07-12 16:02:38 -0400

    inital commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,25 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,FormsModule,CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'my-first-angular-app';
+  title = 'Greg';
+  favoriteTech = 'C#';
+  favColor = "grey";
+  names = [ 'LC', 'Emily', 'Karol', 'Angelica', 'Cheryl' ];
+
+  changeData(): void  {
+    this.title = "Gregory";
+    this.favoriteTech = "Angular";
+    this.favColor = "Green";
+    this.names.push('Denise');
+  }
 }
